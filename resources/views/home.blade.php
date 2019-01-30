619a2b4778f78729eef41a46253c7b6e95b77b58
Sion10032 <sion10032@hotmail.com>
2019-01-30 14:29:02 +0800

    支持全屏“应用程序模式”

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,8 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="full-screen" content="true">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>ACCollection</title>
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
