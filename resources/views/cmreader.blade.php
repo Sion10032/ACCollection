9649053da18ec82432feeb04037861d63bbfd69b
Sion10032 <sion10032@hotmail.com>
2019-02-18 20:11:27 +0800

    1. 添加favicon等

--- resources/views/cmreader.blade.php
+++ resources/views/cmreader.blade.php
@@ -2,10 +2,13 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>AC - CMReader</title>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="full-screen" content="true">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>ACCollection</title>
+        <link rel="bookmark" type="image/png" href="/image/ui/avatar-karen.png">
+        <link rel="apple-touch-icon" href="/image/ui/avatar-karen.png">
+        <link rel="shortcut icon" href="/favicon.ico">
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,13 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="full-screen" content="true">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>ACCollection</title>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="full-screen" content="true">
+        <link rel="bookmark" type="image/png" href="/image/ui/avatar-karen.png">
+        <link rel="apple-touch-icon" href="/image/ui/avatar-karen.png">
+        <link rel="shortcut icon" href="/favicon.ico">
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
