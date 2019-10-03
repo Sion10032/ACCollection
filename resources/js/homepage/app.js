5e077c1bac71283abe61d0e8bb0f6c7600288afd
Sion10032 <Sion10032@hotmail.com>
2019-10-04 02:21:07 +0800

    1. 引入Vant； 2. 修改导航栏。

--- resources/js/homepage/app.js
+++ resources/js/homepage/app.js
@@ -1,30 +1,34 @@
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import VueLocalStorage from 'vue-localstorage'
-import VueAuth from '@websanova/vue-auth'
-import VueAuthAuth from '@websanova/vue-auth/drivers/auth/bearer.js'
-import VueAuthHttp from '@websanova/vue-auth/drivers/http/axios.1.x.js'
-import VueAuthRouter from '@websanova/vue-auth/drivers/router/vue-router.2.x.js'
-import App from './App.vue'
-import router from './routers.js'
-import 'normalize.css'
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VueLocalStorage from 'vue-localstorage';
+import VueAuth from '@websanova/vue-auth';
+import VueAuthAuth from '@websanova/vue-auth/drivers/auth/bearer.js';
+import VueAuthHttp from '@websanova/vue-auth/drivers/http/axios.1.x.js';
+import VueAuthRouter from '@websanova/vue-auth/drivers/router/vue-router.2.x.js';
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import App from './App.vue';
+import router from './routers.js';
+import 'normalize.css';
 
-Vue.use(VueAxios, axios)
-Vue.use(VueLocalStorage)
+Vue.use(VueAxios, axios);
+Vue.use(VueLocalStorage);
 
-Vue.router = router
+Vue.router = router;
 Vue.use(VueAuth, {
     auth: VueAuthAuth,
     http: VueAuthHttp,
     router: VueAuthRouter
-})
+});
 
-axios.defaults.baseURL = '/api/'
-Vue.prototype.$axios = axios
+Vue.use(Vant);
+
+axios.defaults.baseURL = '/api/';
+Vue.prototype.$axios = axios;
 
 new Vue({
     el: '#app',
     router: Vue.router,
     render: app => app(App)
-})
+});
